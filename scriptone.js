349c2447c09f1b81557ea627ca3d7199cfa5f377
ataftan <134144780+ataftan@users.noreply.github.com>
2025-09-10 20:01:34 +0530

    m

--- scriptone.js
+++ scriptone.js
@@ -21,51 +21,54 @@ $("#shuffle").click(function () {
   $(this).text(randomText);
 });
 
-
-$("#dark-mode").on("click", function () {
-  $("body").toggleClass("toggled");
-});
-
-
-$("#dark-mode").on("click", function () {
-  $("#txt").toggleClass("toggled");
-});
-
-$("#dark-mode").on("click", function () {
-  $("button").toggleClass("toggled");
-});
+  
 
 
 
-$("#container").on("click", function () {
-  $(this).toggleClass("toggled");
+// $("#dark-mode").on("click", function () {
+//   $("body").toggleClass("toggled");
+// });
 
-  if ($(this).hasClass("toggled")) {
-    $(".i").each(function () {
-      let label = $(this).data("label") || "we co-create a collective body each limb regenerated cyclically its shapeshifting skin";
-      $(".pre-txt span").html(label);
-    });
 
-    $('.txt').css('color', 'transparent');
-    $('.pre-txt').css('background-color', 'white');
-    $('.pre-txt').css('background-image', 'url("landscape.gif")');
-    $('.pre-txt').css('background-repeat', 'no-repeat');
-    $('.pre-txt').css(' background-size', 'cover');
+// $("#dark-mode").on("click", function () {
+//   $("#txt").toggleClass("toggled");
+// });
+
+// $("#dark-mode").on("click", function () {
+//   $("button").toggleClass("toggled");
+// });
 
 
 
+// $("#container").on("click", function () {
+//   $(this).toggleClass("toggled");
+
+//   if ($(this).hasClass("toggled")) {
+//     $(".i").each(function () {
+//       let label = $(this).data("label") || "we co-create a collective body each limb regenerated cyclically its shapeshifting skin";
+//       $(".pre-txt span").html(label);
+//     });
+
+//     $('.txt').css('color', 'transparent');
+//     $('.pre-txt').css('background-color', 'white');
+//     $('.pre-txt').css('background-image', 'url("landscape.gif")');
+//     $('.pre-txt').css('background-repeat', 'no-repeat');
+//     $('.pre-txt').css(' background-size', 'cover');
 
 
-  } else {
-    $('.txt').css('color', '');
-    $('.pre-txt').css('background-color', '');
-    $('.pre-txt').css('background-image', '');
-    $('.pre-txt').css('background-repeat', '');
-    $('.pre-txt').css(' background-size', '');
-    $('.pre-txt').css('   image-rendering', 'pixelated');
 
-    $('.pre-txt span').html('');
-  }
-});
+
+
+//   } else {
+//     $('.txt').css('color', '');
+//     $('.pre-txt').css('background-color', '');
+//     $('.pre-txt').css('background-image', '');
+//     $('.pre-txt').css('background-repeat', '');
+//     $('.pre-txt').css(' background-size', '');
+//     $('.pre-txt').css('   image-rendering', 'pixelated');
+
+//     $('.pre-txt span').html('');
+//   }
+// });
 
 
